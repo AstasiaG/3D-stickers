d4d65e6f118b40198e5b98fe0b954053bf70d76a
Anastasia <anstasa.li@gmail.com>
2026-06-10 03:30:58 +0300

    feat add sections

---
+++ index.js
@@ -0,0 +1,161 @@
+const menu = document.querySelector('nav.header_bottom');
+const menuLinks = document.querySelector('.header__top_links');
+const burger = document.querySelector('a.burger-menu');
+const sliderItem = Array.from(document.getElementsByClassName('reviews__slider-item'));
+const arrRight = document.querySelector('.reviews__slider-arrow.right');
+const arrLeft = document.querySelector('.reviews__slider-arrow.left');
+const slider = document.querySelector('.reviews__slider');
+let current = 0;
+
+
+let scrollY = 0;
+
+function openModal() {
+  scrollY = window.scrollY;
+
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
+  document.body.style.width = '100%';
+
+  if (window.innerWidth > 1180) {
+    document.body.style.paddingRight = '15px';
+  }
+}
+
+function closeModal() {
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.width = '';
+  document.body.style.paddingRight = '';
+
+  window.scrollTo(0, scrollY);
+}
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger-menu__active');
+  menu.classList.toggle('active');
+  menuLinks.classList.toggle('active');
+
+  if (menu.className.includes('active')) {
+    openModal();
+  } else {
+    closeModal();
+  }
+})
+
+function Left() {
+  let itemWidth = sliderItem[0].offsetWidth;
+  const max = sliderItem.length - 1;
+  const gap = parseInt(getComputedStyle(slider).columnGap);
+  const step = itemWidth + gap;
+
+  if (current > 0) {
+    current--;
+    slider.style.transform = `translateX(-${current * step}px)`;
+  }
+  
+}
+
+function Right() {
+  let itemWidth = sliderItem[0].offsetWidth;
+  const max = (sliderItem.length /2) - 1;
+  const gap = parseInt(getComputedStyle(slider).columnGap);
+  const step = itemWidth + gap;
+
+
+  if (current < max) {
+    current++;
+    slider.style.transform = `translateX(-${current * step}px)`;
+  }
+}
+
+arrLeft.addEventListener( "click" , Left);
+arrRight.addEventListener( "click" , Right);
+
+
+const cards = document.querySelectorAll('.partners__logo');
+const button = document.querySelector('.partners__btn');
+const works = document.querySelectorAll('.works__img');
+const worksButton = document.querySelector('.works__btn');
+
+let startIndex =
+window.innerWidth > 1473 ? 4 :
+window.innerWidth > 1097 ? 3 :
+      4;
+
+let startIndexWorks =
+window.innerWidth > 1302 ? 9 :
+window.innerWidth > 873 ? 4 :
+      4;
+
+window.addEventListener('resize', () => {
+  startIndex =
+  window.innerWidth > 1473 ? 4 :
+  window.innerWidth > 1097 ? 3 :
+        4;
+  
+  startIndexWorks =
+  window.innerWidth > 1302 ? 9 :
+  window.innerWidth > 873 ? 4 :
+        4;
+  
+  hideCards();
+  hideWorks();
+})
+
+function hideCards() {
+  cards.forEach((card, index) => {
+    if (index >= startIndex) {
+      card.classList.add('hidden');
+    } else {
+      card.classList.remove('hidden');
+    }
+  });
+}
+
+function hideWorks() {
+  works.forEach((work, index) => {
+    if (index >= startIndexWorks) {
+      work.classList.add('hidden');
+    } else {
+      work.classList.remove('hidden');
+    }
+  });
+}
+
+hideCards();
+hideWorks();
+
+button.addEventListener('click', () => {
+  const hiddenCards = document.querySelectorAll('.partners__logo.hidden');
+
+  if (hiddenCards.length > 0) {
+    for (let i = 0; i < 4 && i < hiddenCards.length; i++) {
+      hiddenCards[i].classList.remove('hidden');
+    }
+  } else {
+    hideCards();
+    button.textContent = 'Показать еще';
+  }
+
+  if (document.querySelectorAll('.partners__logo.hidden').length === 0) {
+    button.textContent = 'Скрыть';
+  }
+});
+
+worksButton.addEventListener('click', () => {
+  const hiddenCards = document.querySelectorAll('.works__img.hidden');
+
+  if (hiddenCards.length > 0) {
+    for (let i = 0; i < 6 && i < hiddenCards.length; i++) {
+      hiddenCards[i].classList.remove('hidden');
+    }
+  } else {
+    hideWorks();
+    worksButton.textContent = 'Показать еще';
+  }
+
+  if (document.querySelectorAll('.works__img.hidden').length === 0) {
+    worksButton.textContent = 'Скрыть';
+  }
+});
